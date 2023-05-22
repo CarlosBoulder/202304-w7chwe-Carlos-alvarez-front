8f9a8be94bf23563e54405ab84c14600f5df50d2
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-22 15:31:17 +0200

    Add new import for eslint fail

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import appRouter from "./routers/AppRouter";
 import { RouterProvider } from "react-router-dom";
+import appRouter from "./routers/appRouter";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
